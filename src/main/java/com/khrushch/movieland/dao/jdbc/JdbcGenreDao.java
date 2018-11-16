5a7497f0f222f29f9b4bfea230379d86afac5cfb
unknown <admin@sand001.luxoft.com>
2018-11-16 20:40:40 +0200

    Separated Genre RowMapper into a separate class

--- src/main/java/com/khrushch/movieland/dao/jdbc/JdbcGenreDao.java
+++ src/main/java/com/khrushch/movieland/dao/jdbc/JdbcGenreDao.java
@@ -1,9 +1,11 @@
 package com.khrushch.movieland.dao.jdbc;
 
 import com.khrushch.movieland.dao.GenreDao;
+import com.khrushch.movieland.dao.jdbc.mapper.GenreRowMapper;
 import com.khrushch.movieland.model.Genre;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,10 +14,11 @@ import java.util.List;
 public class JdbcGenreDao implements GenreDao {
     private static final String SELECT_ALL_GENRES_SQL = "SELECT id, name FROM genre";
 
+    private RowMapper<Genre> genreRowMapper = new GenreRowMapper();
     private JdbcTemplate jdbcTemplate;
 
     public List<Genre> getAll() {
-        return jdbcTemplate.query(SELECT_ALL_GENRES_SQL, (resultSet, i) -> new Genre(resultSet.getLong("id"), resultSet.getString("name")));
+        return jdbcTemplate.query(SELECT_ALL_GENRES_SQL, genreRowMapper);
     }
 
     @Autowired
