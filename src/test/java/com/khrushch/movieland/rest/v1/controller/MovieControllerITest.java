dd5bc6def63cb1d1aa0dfdf90c291d55c9215cbf
unknown <admin@sand001.luxoft.com>
2018-11-18 19:57:53 +0200

    Switched integration tests from mocks to HSQL embedded DB

--- src/test/java/com/khrushch/movieland/rest/v1/controller/MovieControllerITest.java
+++ src/test/java/com/khrushch/movieland/rest/v1/controller/MovieControllerITest.java
@@ -1,19 +1,15 @@
 package com.khrushch.movieland.rest.v1.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.khrushch.movieland.dao.jdbc.JdbcMovieDao;
 import com.khrushch.movieland.model.Movie;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.skyscreamer.jsonassert.comparator.ArraySizeComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -22,13 +18,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -36,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:movie-test-applicationContext.xml", "file:src/main/webapp/WEB-INF/rest-v1-servlet.xml"})
+@ContextConfiguration(locations = {"classpath:test-applicationContext.xml", "file:src/main/webapp/WEB-INF/rest-v1-servlet.xml"})
 @WebAppConfiguration
 public class MovieControllerITest {
     private MockMvc mockMvc;
@@ -51,12 +44,6 @@ public class MovieControllerITest {
 
     @Test
     public void testGetAll() throws Exception {
-        JdbcTemplate mockJdbcTemplate = mock(JdbcTemplate.class);
-        when(mockJdbcTemplate.query(any(String.class), Matchers.<RowMapper<Movie>>any())).thenReturn(getTestMovies());
-
-        JdbcMovieDao jdbcMovieDao = wac.getBean(JdbcMovieDao.class);
-        jdbcMovieDao.setJdbcTemplate(mockJdbcTemplate);
-
         MvcResult mvcResult = mockMvc.perform(get("/movie"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -69,20 +56,10 @@ public class MovieControllerITest {
         String expectedJson = mapper.writeValueAsString(getTestMovies());
 
         JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
-        verify(mockJdbcTemplate, times(1)).query(any(String.class), Matchers.<RowMapper<Movie>>any());
     }
 
     @Test
     public void testGetRandom() throws Exception {
-        List<Movie> mockMovies = Stream.generate(Movie::new)
-                .limit(20)
-                .collect(Collectors.toList());
-        JdbcTemplate mockJdbcTemplate = mock(JdbcTemplate.class);
-        when(mockJdbcTemplate.query(any(String.class), Matchers.<RowMapper<Movie>>any())).thenReturn(mockMovies);
-
-        JdbcMovieDao jdbcMovieDao = wac.getBean(JdbcMovieDao.class);
-        jdbcMovieDao.setJdbcTemplate(mockJdbcTemplate);
-
         MvcResult mvcResult = mockMvc.perform(get("/movie/random"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -92,17 +69,10 @@ public class MovieControllerITest {
         String actualJson = mvcResult.getResponse().getContentAsString();
 
         JSONAssert.assertEquals("[3]", actualJson, new ArraySizeComparator(JSONCompareMode.LENIENT));
-        verify(mockJdbcTemplate, times(1)).query(any(String.class), Matchers.<RowMapper<Movie>>any());
     }
 
     @Test
     public void testGetByGenreId() throws Exception {
-        JdbcTemplate mockJdbcTemplate = mock(JdbcTemplate.class);
-        when(mockJdbcTemplate.query(any(String.class), Matchers.<RowMapper<Movie>>any(), anyLong())).thenReturn(getTestMovies());
-
-        JdbcMovieDao jdbcMovieDao = wac.getBean(JdbcMovieDao.class);
-        jdbcMovieDao.setJdbcTemplate(mockJdbcTemplate);
-
         MvcResult mvcResult = mockMvc.perform(get("/movie/genre/1"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -112,10 +82,12 @@ public class MovieControllerITest {
         String actualJson = mvcResult.getResponse().getContentAsString();
 
         ObjectMapper mapper = new ObjectMapper();
-        String expectedJson = mapper.writeValueAsString(getTestMovies());
+        List<Movie> moviesWithGenreIdEqual1 = getTestMovies().stream()
+                .filter(m -> Arrays.asList(1L, 2L, 4L).contains(m.getId()))
+                .collect(Collectors.toList());
+        String expectedJson = mapper.writeValueAsString(moviesWithGenreIdEqual1);
 
         JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
-        verify(mockJdbcTemplate, times(1)).query(any(String.class), Matchers.<RowMapper<Movie>>any(), anyLong());
     }
 
     private List<Movie> getTestMovies() {
@@ -137,7 +109,25 @@ public class MovieControllerITest {
         second.setPrice(145.9);
         second.setPicturePath("https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1._SY209_CR0,0,140,209_.jpg");
 
-        return new ArrayList<>(Arrays.asList(first, second));
+        Movie third = new Movie();
+        third.setId(3);
+        third.setRussianName("Побег из Шоушенка");
+        third.setNativeName("The Shawshank Redemption");
+        third.setYearOfRelease(1994);
+        third.setRating(8.9);
+        third.setPrice(123.45);
+        third.setPicturePath("https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1._SY209_CR0,0,140,209_.jpg");
+
+        Movie fourth = new Movie();
+        fourth.setId(4);
+        fourth.setRussianName("Зеленая миля");
+        fourth.setNativeName("The Green Mile");
+        fourth.setYearOfRelease(1999);
+        fourth.setRating(8.6);
+        fourth.setPrice(134.67);
+        fourth.setPicturePath("https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1._SY209_CR0,0,140,209_.jpg");
+
+        return Arrays.asList(first, second, third, fourth);
     }
 
 }
--- src/test/resources/db/sql/insert-data.sql
+++ src/test/resources/db/sql/insert-data.sql
@@ -1,1 +1,27 @@
-insert into role_permission values(1, 'USER', 'http://')
+INSERT INTO movie (id, name, original_name, year, rating, price, poster_url)
+VALUES
+(
+  (1, 'Список Шиндлера', 'Schindler''s List', 1993, 8.7, 150.5, 'https://images-na.ssl-images-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1._SX140_CR0,0,140,209_.jpg')
+, (2, 'Унесённые призраками', 'Sen to Chihiro no kamikakushi', 2001, 8.6, 145.9, 'https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1._SY209_CR0,0,140,209_.jpg')
+, (3, 'Побег из Шоушенка', 'The Shawshank Redemption', 1994, 8.9, 123.45, 'https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1._SY209_CR0,0,140,209_.jpg')
+, (4, 'Зеленая миля', 'The Green Mile', 1999, 8.6, 134.67, 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1._SY209_CR0,0,140,209_.jpg')
+);
+
+INSERT INTO genre (id, name)
+VALUES
+(
+  (1, 'вестерн')
+, (2, 'ужасы')
+);
+
+INSERT INTO movie_genre (id, movie_id, genre_id)
+VALUES
+(
+  (1, 1, 1)
+, (2, 1, 2)
+, (3, 2, 1)
+, (4, 3, 2)
+, (5, 4, 1)
+, (6, 4, 2)
+);
+
