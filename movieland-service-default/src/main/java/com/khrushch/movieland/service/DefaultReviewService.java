6de7b77aaac5a69c6029516cd3e2b2ad524c7bf7
Your Name <you@example.com>
2018-12-08 13:06:54 +0200

    [b-8] Removed user role check from DefaultReviewService after switching security to ProtectedBy annotation

--- movieland-service-default/src/main/java/com/khrushch/movieland/service/DefaultReviewService.java
+++ movieland-service-default/src/main/java/com/khrushch/movieland/service/DefaultReviewService.java
@@ -1,12 +1,8 @@
 package com.khrushch.movieland.service;
 
 import com.khrushch.movieland.dao.ReviewDao;
-import com.khrushch.movieland.holder.CurrentUserHolder;
 import com.khrushch.movieland.model.Movie;
 import com.khrushch.movieland.model.Review;
-import com.khrushch.movieland.model.User;
-import com.khrushch.movieland.model.UserRole;
-import com.khrushch.movieland.service.exception.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,13 +14,6 @@ public class DefaultReviewService implements ReviewService {
 
     @Override
     public void addReview(Review review) {
-        User user = CurrentUserHolder.getUser();
-        if (user == null) {
-            throw new AuthenticationException("Not authorized, user not found");
-        } else if (user.getRole() != UserRole.USER){
-            throw new AuthenticationException("Not enough privileges");
-        }
-
         reviewDao.addReview(review);
     }
 
